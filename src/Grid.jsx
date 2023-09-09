dfe23d3b5b023d0d37c8b5732eeef06d65a8c7ef
Oluwasetemi <setemiojo@gmail.com>
2023-09-09 12:54:09 -0500

    2 files modified - Grid.jsx amd utils.js modified

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,7 +1,27 @@
 import { range } from './utils';
 
+const gridStyles = {
+  width: '100px',
+  height: '100px',
+  border: 'solid grey',
+  borderRadius: '5px',
+};
+
 function Grid({ numRows, numCols }) {
-  return <div className="grid">{/* TODO */}</div>;
+  return (
+    <div style={{}} className="grid">
+      {range(numRows).map((row) => (
+        <div
+          key={row}
+          style={{ display: 'flex', gap: '4px', marginBottom: '4px' }}
+        >
+          {range(numCols).map((column) => (
+            <div key={column} style={gridStyles}></div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Grid;
